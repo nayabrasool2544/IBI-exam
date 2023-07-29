f9e4fd79cd7199386560fd18c7a285e043048bda
nayabrasool2544 <111410746+nayabrasool2544@users.noreply.github.com>
2023-07-29 20:51:18 +0530

    Create Accumulator.js

---
+++ Accumulator.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.reduce((acc, num) => {
+  if (num % 2 === 0) {
+    acc.evens.push(num);
+  } else {
+    acc.odds.push(num);
+  }
+  return acc;
+}, { evens: [], odds: [] });
+
+console.log(result);
